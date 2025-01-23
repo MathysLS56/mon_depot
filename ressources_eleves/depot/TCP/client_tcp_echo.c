ec33bc1cc76c5b575b09ba9a044881702247e055
elouan23 <legall.e1@stjolorient.fr>
2025-01-23 12:02:05 +0100

    Update client_tcp_echo.c

--- ressources_eleves/depot/TCP/client_tcp_echo.c
+++ ressources_eleves/depot/TCP/client_tcp_echo.c
@@ -7,14 +7,19 @@
 #include <stdlib.h>
 #include <string.h>
 #include <netinet/in.h>
-
+void traiter_commande(int argc, char** argv, const char* usage) {
+    if (argc != 5) {
+        printf(stderr, "%s\n", usage);
+    }
 int main(int argc, char** argv) {
-	//traiter_commande(/* A COMPLETER */, argv[0], "<adresse IP> <port> <message> <nb occurences>\nmauvais nombre d'arguments");
-	//traiter_commande(/* A COMPLETER */, argv[0], "<adresse IP> <port> <message> <nb occurences>\n<adresse IP> est une adresse IP au format décimal pointé");
-	//traiter_commande(/* A COMPLETER */, argv[0], "<adresse IP> <port> <message> <nb occurences>\n<port> est un port non réservé");
-	//traiter_commande(/* A COMPLETER */, argv[0], "<adresse IP> <port> <message> <nb occurences>\n<nb occurences> est un entier positif");
+	
+	
+	traiter_commande(, argv[0], "<adresse IP> port:1023<<message> <nb occurences>\nmauvais nombre d'arguments");
+	traiter_commande(, argv[0], "<adresse IP> <port> <message> <nb occurences>\n<adresse IP> est une adresse IP au format décimal pointé");
+	traiter_commande(, argv[0], "<adresse IP> <port> <message> <nb occurences>\n<port> est un port non réservé");
+	traiter_commande(, argv[0], "<adresse IP> <port> <message> <nb occurences>\n<nb occurences> est un entier positif");
 
-	/*A COMPLETER*/
+	
 
 	exit(0);
 }
