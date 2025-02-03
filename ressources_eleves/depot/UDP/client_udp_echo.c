f4a253c348029d099f0aac7bf2ab732890f84653
elouan23 <legall.e1@stjolorient.fr>
2025-02-03 09:54:53 +0100

    Update client_udp_echo.c

--- ressources_eleves/depot/UDP/client_udp_echo.c
+++ ressources_eleves/depot/UDP/client_udp_echo.c
@@ -5,16 +5,48 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include <string.h>
 #include <netinet/in.h>
 
-int main(int argc, char** argv) {
-	//traiter_commande(/*A COMPLETER*/, argv[0], "<adresse IP> <port> <message> <nb occurences>\nmauvais nombre d'arguments");
-	//traiter_commande(/*A COMPLETER*/, argv[0], "<adresse IP> <port> <message> <nb occurences>\n<adresse IP> est une adresse IP au format décimal pointé");
-	//traiter_commande(/*A COMPLETER*/, argv[0], "<adresse IP> <port> <message> <nb occurences>\n<port> est un port non réservé");
-	//traiter_commande(/*A COMPLETER*/, argv[0], "<adresse IP> <port> <message> <nb occurences>\n<nb occurences> est un entier positif");
+void traiter_commande(int argc, char** argv, const char* usage) {
+    if (argc != 5) {
+        fprintf(stderr, "%s\n", usage);
+        exit(EXIT_FAILURE);
+    }
 
-	/*A COMPLETER*/
+    // Validate IP address
+    struct sockaddr_in sa;
+    if (inet_pton(AF_INET, argv[1], &(sa.sin_addr)) != 1) {
+        fprintf(stderr, "%s\n", usage);
+        exit(EXIT_FAILURE);
+    }
 
-	exit(0);
+    // Validate port
+    int port = atoi(argv[2]);
+    if (port <= 1024 || port > 65535) {
+        fprintf(stderr, "%s\n", usage);
+        exit(EXIT_FAILURE);
+    }
+
+    // Validate number of occurrences
+    int nb_occurrences = atoi(argv[4]);
+    if (nb_occurrences <= 0) {
+        fprintf(stderr, "%s\n", usage);
+        exit(EXIT_FAILURE);
+    }
+
+    // If all validations pass, proceed with the message
+    printf("IP Address: %s\n", argv[1]);
+    printf("Port: %d\n", port);
+    printf("Message: %s\n", argv[3]);
+    printf("Number of occurrences: %d\n", nb_occurrences);
 }
+
+int main(int argc, char** argv) {
+    traiter_commande(argc, argv, "Usage: <adresse IP> <port> <message> <nb occurences>\n"
+                                "<adresse IP> est une adresse IP au format décimal pointé\n"
+                                "<port> est un port non réservé\n"
+                                "<nb occurences> est un entier positif");
+
+    exit(0);
+}
+
