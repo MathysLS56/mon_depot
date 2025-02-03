70d7def7618893ad334716fdd613abbee3ab3b19
MathysLS56 <mathystest56@gmail.com>
2025-02-03 10:33:28 +0100

    Version finale du serveur_udp_echo.c

--- ressources_eleves/depot/UDP/serveur_udp_echo.c
+++ ressources_eleves/depot/UDP/serveur_udp_echo.c
@@ -6,76 +6,73 @@
 #include <stdlib.h>
 #include <string.h>
 #include <netinet/in.h>
-
+#include <arpa/inet.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <unistd.h>
 
+void traiter_commande(int condition, const char* progname, const char* msg) {
+    if (!condition) {
+        fprintf(stderr, "Usage: %s %s\n", progname, msg);
+        exit(1);
+    }
+}
+
 int main(int argc, char** argv) {
-	traiter_commande(/*A COMPLETER*/, argv[0], "<port>\nmauvais nombre d'arguments");
-	traiter_commande(/*A COMPLETER*/, argv[0], "<port>\n<port> est un port non réservé");
+    traiter_commande(argc == 2, argv[0], "<port>\nmauvais nombre d'arguments");
+
+    int port = atoi(argv[1]);
+    traiter_commande(port >= 1024 && port <= 65535, argv[0], "<port>\n<port> est un port non réservé");
 
 //----------------prototype socket
+    int sock = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sock == -1) {
+        printf("Echec creation socket\n");
+        exit(1);
+    }
 
-int socket(int domain, int type, int protocol);
-int close(int sock);
-ssize_t sendto(int sock, const void *buf, size_t len, int flags,
-const struct sockaddr *dest_addr, socklen_t addr_len);
-ssize_t recvfrom(int sock, void *buf, size_t len, int flags,
-struct sockaddr *src_addr, socklen_t *addr_len);
+   
+    struct sockaddr_in server_addr;
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(port);
+    server_addr.sin_addr.s_addr = INADDR_ANY;
 
+    
+    if (bind(sock, (struct sockaddr*) &server_addr, sizeof(server_addr)) == -1) {
+        perror("Echec creation socket");
+        close(sock);
+        exit(1);
+    }
 
+    printf("Serveur écoute sur le port %d\n", port);
 
-int sock ;
-sock = socket(AF_INET, SOCK_DGRAM, 0);
+    while (1) {
+        
+        struct sockaddr_in client_addr;
+        socklen_t addr_len = sizeof(client_addr);
+        char buf[1024];
+        ssize_t recv_len = recvfrom(sock, buf, sizeof(buf) - 1, 0, (struct sockaddr*) &client_addr, &addr_len);
+        if (recv_len == -1) {
+            perror("message non reçu");
+            close(sock);
+            exit(1);
+        }
 
-if (sock == -1){
-	printf(« Echec creation socket\n »);
-	exit(1);}
+        buf[recv_len] = '\0';
+        printf("Message reçu de %s:%d: %s\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port), buf);
 
-//--------------------envoi
+        // reponse
+        const char* response = "Message reçu";
+        if (sendto(sock, response, strlen(response), 0, (struct sockaddr*) &client_addr, addr_len) == -1) {
+            perror("réponse non envoyé");
+            close(sock);
+            exit(1);
+        }
+    }
 
-...
-struct sockaddr_in dest_addr ;
-...
-// on définit le message à envoyer
-char* msg = "coucou";
+    // Fermeture du socket
+    close(sock);
 
-// on initialise la structure d'adresse
-bzero(&dest_addr, sizeof(dest_addr));
-
-// on initialise la structure d'adresse avec les valeurs souhaitées
-dest_addr.sin_family = AF_INET ;
-dest_addr.sin_port = htons(5896) ;
-inet_aton("192.168.1.3", &dest_addr .sin_addr) ;
-
-// on appelle la fonction (on suppose que « sock » est initialisée)
-if (sendto (sock, msg, strlen(msg), 0,
-(struct sockaddr*) &dest_addr, sizeof(dest_addr)) == -1) {
-printf("Echec envoi\n") ;
-exit(1) ;
-}
-
-//------------------reception
-
-...
-struct sockaddr_in src_addr ;
-socklen_t addr_len ;
-...
-// on définit un tableau de réception
-char buf[32] ;
-
-// on appelle la fonction (on suppose que « sock » est initialisée)
-addr_len = sizeof(src_addr) ;
-if (recvfrom (sock, buf, sizeof(buf), 0,
-(struct sockaddr*) &src_addr, &addr_len) == -1) {
-printf(« Echec réception\n ») ;
-exit(1) ;
-}
-
-//----------------fermeture du socket
-close(sock);
-
-
-	exit(0);
+    return 0;
 }
